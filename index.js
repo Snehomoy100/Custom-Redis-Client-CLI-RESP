f46c41b7f211cd74de777bb4ea5fac2e507197fd
snehomoy100 <snehomoy100@gmail.com>
2024-06-09 22:10:24 +0530

    feat: get & set functionality added, which stores the in form of kv pair in a simple hashmap as of now

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const net = require('net');
+const Parser = require('redis-parser');
 
+const PORT = process.env.PORT || 5556;
 
-const PORT = process.env.PORT || 5555;
+const CACHE_STORAGE = {};
 
 const server = net.createServer(connection => {
     console.log("Client connected...!");
 
-    // to get the data from redis-cli to this client
+    // to get & set the data from redis-cli to this client
     connection.on('data', data => {
-        console.log('=>', data.toString());
+        const parser = new Parser({
+            returnReply: (reply) => {
+                const command = reply[0];
+                switch(command){
+                    case 'set': {
+                        const key = reply[1];
+                        const value = reply[2];
+                        CACHE_STORAGE[key] = value;
+                        connection.write('+OK\r\n');
+                    }
+                        break;
+                    
+                    case 'get': {
+                        const key = reply[1];
+                        const value = CACHE_STORAGE[key];
+                        if(!value) {
+                            connection.write('$-1\r\n');
+                        } else {
+                            connection.write(`$${value.length}\r\n${value}\r\n`);
+                        }
+                    }
+                        
+                }
+            },
+            returnError: (error) => {
+                console.log('=>', error);
+            }
+        })
 
-        // to send back data to redis-cli as acknowledgement from custom client
+        parser.execute(data);
+
+        // to send back an acknowledgement to redis-cli from custom client
         connection.write('+OK\r\n');
     });
 })
