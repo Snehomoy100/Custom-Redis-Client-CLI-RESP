b7c202787a8b3c740d3d6937270d4bf71d282581
snehomoy100 <snehomoy100@gmail.com>
2024-06-09 21:35:16 +0530

    feat: Client connected with redis-cli & getting the expected data

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const net = require('net');
 
+
+const PORT = process.env.PORT || 5555;
+
 const server = net.createServer(connection => {
     console.log("Client connected...!");
+
+    // to get the data from redis-cli to this client
+    connection.on('data', data => {
+        console.log('=>', data.toString());
+    });
 })
+
+server.listen(PORT, () => console.log(`Custom Redis Server is running on port ${PORT}`));
+
